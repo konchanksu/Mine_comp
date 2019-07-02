710696d660a88fc1e97ede20ee78a1c0b60de983
konchanksu <nyankomi59@gmail.com>
2019-07-02 11:38:29 +0900

    とりあえずの完成系（ただし変更はまだ加える）

---
+++ MineSweeper_ver_3.0/Click.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <handy.h>
+
+int mapsizex_ex;  //配列内のサイズ
+int mapsizey_ex;  //配列内のサイズ
+int bombnum_ex;   //爆弾の個数
+int Coordinatex_ex;//マスの左下の角のx座標
+int Coordinatey_ex;//マスの左下の角のy座標
+int boxsize;
+int boxcenter;
+
+//クリックした場所の判断
+int ClickX_C(int x){
+  int tmpX = 1000 - Coordinatex_ex;
+  if(x>Coordinatex_ex && x<tmpX){
+    int arrayx;
+    arrayx =(x - Coordinatex_ex) / boxsize;
+    return arrayx;
+  }
+  return -1;
+}
+int ClickY_C(int y){
+  int tmpY = 700 - Coordinatey_ex;
+  if(y>Coordinatey_ex && y<tmpY){
+    int arrayy;
+    arrayy =(y - Coordinatey_ex) / boxsize;
+    return arrayy;
+  }
+  return -1;
+}
