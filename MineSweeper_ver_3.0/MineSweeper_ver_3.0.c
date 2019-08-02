051f5d3f1627c4a33c30e9f1325c92f084e69ef4
konchanksu <nyankomi59@gmail.com>
2019-08-02 13:04:38 +0900

    インデント変更

--- MineSweeper_ver_3.0/MineSweeper_ver_3.0.c
+++ MineSweeper_ver_3.0/MineSweeper_ver_3.0.c
@@ -20,9 +20,9 @@ extern int boxcenter;     //マス一マスの半分のサイズ
 //_ex --extern
 
 /*
-　　マインスイーパー　配列の振り分け
-　　　旗の付いていないマス
-　　　　爆弾 ... 10
+   　　マインスイーパー　配列の振り分け
+   　　　旗の付いていないマス
+   　　　　爆弾 ... 10
      　開いていない番号のマス ... それぞれの周りにある爆弾の数
      　開いた番号のマス　...　-100
 
@@ -30,91 +30,106 @@ extern int boxcenter;     //マス一マスの半分のサイズ
      　爆弾 ... -10
      　開いていない番号のマス（０を除く） ...　-番号
      　開いていない0のマス ... -20
-*/
+ */
 
 int main(){
-  srand(time(NULL));
-  int x, y;           //クリック位置
-  int explain_route;  //説明に行くかどうか
-  int level;          //レベル設定
-  int remain;         //クリアまで残り~
-  int flag = 0;       //旗のon off
-  int layer1, layer2; //レイヤー設定
-  int arrayx, arrayy; //配列での座標
-  int forecastbom;    //残り爆弾の数（プレーヤーが予測した数）
-  hgevent *event;
+        srand(time(NULL));
+        //変数の宣言
+        int x, y;     //クリック位置
+        int explain_route; //説明に行くかどうか
+        int level;    //レベル設定
+        int remain;   //クリアまで残り~
+        int flag = 0; //旗のon off
+        int layer1, layer2; //レイヤー設定
+        int arrayx, arrayy; //配列での座標
+        int forecastbom; //残り爆弾の数（プレーヤーが予測した数）
+        hgevent *event;
 
-  boxsize = 30;
-  boxcenter = 15;
 
-  HgOpen(1000, 700);
-  HgSetEventMask(HG_MOUSE_DOWN);
+        boxsize = 30;
+        boxcenter = 15;
 
-  layer1 = HgWAddLayer(0);
-  layer2 = HgWAddLayer(0);
+        HgOpen(1000, 700);
+        HgSetEventMask(HG_MOUSE_DOWN);
 
-  explain_route = Opening_D(x,y,event);
-  level = Explain_D(x,y,event,explain_route);
-  LevelDecide_A(level);
+        layer1 = HgWAddLayer(0);
+        layer2 = HgWAddLayer(0);
 
-  int MAP[mapsizex_ex][mapsizey_ex];
-  int DONOTCHANGE[mapsizex_ex][mapsizey_ex];
-  RandomBomb_A(MAP);
-  DontChange(MAP,DONOTCHANGE);
-  GameUI_D();
+        //オープニング画面、レベル設定画面
+        explain_route = Opening_D(x,y,event);
+        level = Explain_D(x,y,event,explain_route);
+        LevelDecide_A(level);
 
-  forecastbom = bombnum_ex;
-  remain = mapsizex_ex * mapsizey_ex - bombnum_ex;
-  Forecastbom_D(forecastbom, layer1);
 
-  HgSleep(0.3);
-  int startopen = StOpen_A(MAP);
-  if(startopen != -1){
-    int starty = startopen/100;
-    int startx = startopen - (starty * 100);
-    HgSetFont(HG_G,20);
-    int wherex = Coordinatex_ex + startx*boxsize, wherey = Coordinatey_ex + starty*boxsize;
-    HgSetFillColor(HG_WHITE);
-    HgBoxFill(wherex,wherey,boxsize,boxsize,1);
-    MAP[startx][starty] = -100;
-    remain--;
-    remain = dfs_D(startx, starty, MAP, remain);
-  }
+        //爆弾の配置
+        int MAP[mapsizex_ex][mapsizey_ex];
+        int DONOTCHANGE[mapsizex_ex][mapsizey_ex];
+        RandomBomb_A(MAP);
+        DontChange(MAP,DONOTCHANGE);
+        GameUI_D();
 
-  HgSetFont(HG_G,20);
+        forecastbom = bombnum_ex;
+        remain = mapsizex_ex * mapsizey_ex - bombnum_ex;
+        Forecastbom_D(forecastbom, layer1);
 
-  //動作
-  for(;;){
-    //通常モードの操作
-    while(flag==0){
-      event = HgEvent();
-      x = event->x;
-      y = event->y;
-      flag = Flag_D(flag,layer2,x,y);
-      arrayx = ClickX_C(x);
-      arrayy = ClickY_C(y);
-      remain = ClickDiscription_D(arrayx,arrayy,MAP,DONOTCHANGE,remain);
-      //printf("%d\n",remain);
-      forecastbom = ForecastbomR_D(forecastbom, MAP);
-      Forecastbom_D(forecastbom, layer1);
-      if(remain >= 1000)break;
-    }
 
-    //旗をつけるモードの操作
-    while(flag==1){
-      event = HgEvent();
-      x = event->x;
-      y = event->y;
-      flag = Flag_D(flag,layer2,x,y);
-      arrayx = ClickX_C(x);
-      arrayy = ClickY_C(y);
-      forecastbom = FlagInstall_A(MAP,arrayx,arrayy,forecastbom);
-      Forecastbom_D(forecastbom, layer1);
-    }
-    if(remain >= 1000)break;
-  }
-  HgGetChar();
-  HgClose();
+        HgSleep(0.3);
 
-  return 0;
+        //最初のマスを自動で開く動作
+        int startopen = StOpen_A(MAP);
+
+        HgSetFont(HG_G,20);
+
+        if(startopen != -1) {
+                int starty = startopen/100;
+                int startx = startopen - (starty * 100);
+                int wherex = Coordinatex_ex + startx*boxsize, wherey = Coordinatey_ex + starty*boxsize;
+
+                HgSetFillColor(HG_WHITE);
+                HgBoxFill(wherex,wherey,boxsize,boxsize,1);
+
+                MAP[startx][starty] = -100;
+                remain--;
+                remain = dfs_D(startx, starty, MAP, remain);
+        }
+
+        //動作
+        for(;;) {
+                //通常モードの操作
+                while(flag==0) {
+                        event = HgEvent();
+                        x = event->x;
+                        y = event->y;
+
+                        flag = Flag_D(flag,layer2,x,y);
+                        arrayx = ClickX_C(x);
+                        arrayy = ClickY_C(y);
+                        remain = ClickDiscription_D(arrayx,arrayy,MAP,DONOTCHANGE,remain);
+                        //printf("%d\n",remain);
+                        forecastbom = ForecastbomR_D(forecastbom, MAP);
+                        Forecastbom_D(forecastbom, layer1);
+
+                        if(remain >= 1000) break;
+                }
+
+                //旗をつけるモードの操作
+                while(flag==1) {
+                        event = HgEvent();
+                        x = event->x;
+                        y = event->y;
+
+
+                        flag = Flag_D(flag,layer2,x,y);
+                        arrayx = ClickX_C(x);
+                        arrayy = ClickY_C(y);
+                        forecastbom = FlagInstall_A(MAP,arrayx,arrayy,forecastbom);
+                        Forecastbom_D(forecastbom, layer1);
+                }
+
+                if(remain >= 1000) break;
+        }
+        HgGetChar();
+        HgClose();
+
+        return 0;
 }
