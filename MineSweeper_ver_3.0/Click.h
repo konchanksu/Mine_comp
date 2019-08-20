0cb823929d7d68b709a8dfe484692b24e848ec1c
konchanksu <nyankomi59@gmail.com>
2019-08-20 17:04:52 +0900

    インデント調整

--- MineSweeper_ver_3.0/Click.h
+++ MineSweeper_ver_3.0/Click.h
@@ -14,8 +14,8 @@ int Coordinatey_ex;//マスの左下の角のy座標
 int ClickX_C(int x){
     int tmpX = 1000 - Coordinatex_ex;
     int arrayx;
-    if(x>Coordinatex_ex && x<tmpX) {
-        arrayx =(x - Coordinatex_ex) / BOXSIZE;
+    if(x > Coordinatex_ex && x < tmpX){
+        arrayx = (x - Coordinatex_ex) / BOXSIZE;
         return arrayx;
     }
     return -1;
@@ -24,7 +24,7 @@ int ClickX_C(int x){
 int ClickY_C(int y){
     int tmpY = 700 - Coordinatey_ex;
     int arrayy;
-    if(y>Coordinatey_ex && y<tmpY) {
+    if(y > Coordinatey_ex && y < tmpY){
             arrayy =(y - Coordinatey_ex) / BOXSIZE;
             return arrayy;
     }
